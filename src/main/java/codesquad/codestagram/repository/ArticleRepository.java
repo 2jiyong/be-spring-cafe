b39c72bf48e1c5e24f05480a7ea7105d39299dcb
2jiyong <jdragon2000@naver.com>
2025-04-04 15:06:33 +0900

    Feat: Repository에 Pageable로 찾기 추가

--- src/main/java/codesquad/codestagram/repository/ArticleRepository.java
+++ src/main/java/codesquad/codestagram/repository/ArticleRepository.java
@@ -2,9 +2,12 @@ package codesquad.codestagram.repository;
 
 import codesquad.codestagram.domain.Article;
 import codesquad.codestagram.domain.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
+
 
 public interface ArticleRepository {
     Article save(Article article);
@@ -12,5 +15,6 @@ public interface ArticleRepository {
     List<Article> findByTitle(String title);
     Optional<Article> findById(Long id);
     List<Article> findAll();
+    Page<Article> findAll(Pageable pageable);
     void delete(Article article);
 }
--- src/main/java/codesquad/codestagram/repository/ReplyRepository.java
+++ src/main/java/codesquad/codestagram/repository/ReplyRepository.java
@@ -2,6 +2,8 @@ package codesquad.codestagram.repository;
 
 import codesquad.codestagram.domain.Article;
 import codesquad.codestagram.domain.Reply;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
@@ -10,5 +12,6 @@ public interface ReplyRepository {
     Reply save(Reply reply);
     List<Reply> findByArticle(Article article);
     Optional<Reply> findById(Long id);
+    Page<Reply> findAll(Pageable pageable);
     void delete(Reply reply);
 }
